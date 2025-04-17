53211296f40f9bf40c150d449fe23e71d870bdd3
motetpaper <194758925+motetpaper@users.noreply.github.com>
2025-04-17 14:04:31 -0700

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -29,21 +29,55 @@ self.addEventListener('install', (evt) => {
 
 // pwa works offline
 self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    (async () => {
-      const r = await caches.match(evt.request);
-      console.log(`[sw.js] Fetching resource: ${evt.request.url}`);
-      console.log(evt);
-      if (r) {
-        return r;
-      }
-      const response = await fetch(evt.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[sw.js] Caching new resource: ${evt.request.url}`);
-      cache.put(evt.request, response.clone());
-      return response;
-    })(),
-  );
+
+  // the following if/else block tries to fix
+  // this the following error
+  //
+  // TypeError: Failed to execute 'put' on 'Cache':
+  // Request method 'POST' is unsupported
+  //
+
+  const ga = 'https://www.google-analytics.com';
+
+  if(evt.request.url.includes(ga)) {
+    evt.respondWith(fetch(evt.request));
+  } else {
+    evt.respondWith(
+      (async () => {
+        const r = await caches.match(evt.request);
+        console.log(`[sw.js] Fetching resource: ${evt.request.url}`);
+        console.log(evt);
+        if (r) {
+          return r;
+        }
+        const response = await fetch(evt.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[sw.js] Caching new resource: ${evt.request.url}`);
+        cache.put(evt.request, response.clone());
+        return response;
+      })(),
+    );
+  }
 
 // onfetch
+});
+
+
+// pwa upgrades cache
+self.addEventListener('activate', (event) => {
+  const cacheAllowlist = [ cacheName ];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheVersion) => {
+          if (!cacheAllowlist.includes(cacheVersion)) {
+            console.log(`[sw.js] Updating cache to ${cacheVersion} ... `);            
+            return caches.delete(cacheVersion);
+          }
+        }),
+      );
+    }),
+  );
+
+// onactivate
 });
